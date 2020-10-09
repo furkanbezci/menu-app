3c6791e3ed4a441cd86fddfcefb4e14853cecee2
Furkan <baburfurkan1@gmail.com>
2020-10-09 19:17:47 +0300

    Updates

---
+++ precache-manifest.ba3b2df3341cf5cd458ad6d8a4ec4d64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8c91fc1b01ad1e97334ad8fcfa0fb9",
+    "url": "/furkanbezci/menu-app/index.html"
+  },
+  {
+    "revision": "7f4df47d84c787edb3f6",
+    "url": "/furkanbezci/menu-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5472996a93f078041c08",
+    "url": "/furkanbezci/menu-app/static/js/2.1b5cacb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/furkanbezci/menu-app/static/js/2.1b5cacb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f4df47d84c787edb3f6",
+    "url": "/furkanbezci/menu-app/static/js/main.f8eb004f.chunk.js"
+  },
+  {
+    "revision": "dc07dc4e713f1580ba59",
+    "url": "/furkanbezci/menu-app/static/js/runtime-main.f219e774.js"
+  }
+]);
